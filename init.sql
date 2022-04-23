697a42a6bbb7c056525bdefe82da5b2ae7e90f27
Özgür Akıncı <akincior@gmail.com>
2022-04-24 01:52:51 +0300

    init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE
     s_parameter
 (
-    id              INT NOT NULL AUTO_INCREMENT,
-    parameter_code  VARCHAR(25) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    parameter_code VARCHAR(25) NOT NULL,
     parameter_value text,
     CONSTRAINT pk_s_parameter PRIMARY KEY (id)
 );
